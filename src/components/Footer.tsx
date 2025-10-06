3a538f4ec08901c7d2e9442123594af6b1931f4b
Blackinzs <kleber.ramos85@gmail.com>
2025-10-06 18:29:39 -0300

    att contatos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="bg-solidario-blue text-white py-6">
       <div className="container mx-auto px-6">
@@ -13,16 +12,16 @@ const Footer = () => {
               Entre em contato
             </Link>
           </div>
-          
+
           <div className="text-center md:text-left mb-4 md:mb-0">
-            <p>solidariomaisdministracao@solidario.com.br</p>
+            <p>solidariomais@gmail.com</p>
           </div>
-          
+
           <div className="text-center md:text-right">
             <p>(11) 99999-9999</p>
           </div>
         </div>
-        
+
         <div className="border-t border-white/20 mt-6 pt-6 text-center">
           <p>© {currentYear} - Todos direitos reservados</p>
         </div>
