73c5b12d596a5a005737f3c31e6bf76b1dfeaa89
Saulo Cunha <167145321+Saulog3@users.noreply.github.com>
2025-10-07 00:23:57 -0300

    update payment metod 6

--- src/pages/DonateProcess.tsx
+++ src/pages/DonateProcess.tsx
@@ -130,14 +130,7 @@ const DonateProcess = () => {
 
 
       // Redirecionar para o Mercado Pago
-      // window.location.href = paymentData.response.url;
-      let redirectUrl = paymentData.response.url;
-      if (redirectUrl.includes('?')) {
-        redirectUrl += `&external_reference=${donationData.id}`;
-      } else {
-        redirectUrl += `?external_reference=${donationData.id}`;
-      }
-      window.location.href = redirectUrl;
+      window.location.href = paymentData.response.url;
       
       
     } catch (error: any) {
